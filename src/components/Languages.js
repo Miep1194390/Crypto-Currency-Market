b5f6e8634e69364b1c0ffa9f1ac4f078be670557
Miep <1194390@student.roc-nijmegen.nl>
2022-05-01 03:32:35 +0200

    Changed Languages, Personalinfo, Quotes, TopContext

--- src/components/Languages.js
+++ src/components/Languages.js
@@ -2,16 +2,20 @@ import React from "react";
 import "./Languages.css";
 
 function Languages() {
+
   return (
     <div className="MainLanguages">
-      <div>
-        <h1>
-            <h1 className="MyFavorite">My Favorite</h1>
-            <h1 className="Web">Web</h1> 
-            <h1 className="And">And</h1>
-            <h1 className="Program">Program</h1>
-            <h1 className="Languages">Languages</h1>
-          </h1>
+      <div className="GreyContainer">
+          <div className="HeaderLanguages">Project Management & Digital Production</div>
+          <div className="MiddleLanguages">Turning ideas into effective, on-brand deliverables.</div>
+      </div>
+      <div className="GreyContainer">
+          <div className="HeaderLanguages">Web Application Development</div>
+          <div className="MiddleLanguages">Building applications online, unlike software programs.</div>
+      </div>
+      <div className="GreyContainer">
+          <div className="HeaderLanguages">UX Designer & UI Designer</div>
+          <div className="MiddleLanguages">Creating innovative, efficient designs.</div>
       </div>
     </div>
   );
